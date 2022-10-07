2ca323240834d3287d27b1ef8c8873c78574973b
ChrisEllisSains <christopher.ellis@sainsburys.co.uk>
2022-10-07 12:22:22 +0100

    added reactive elements

--- src/main/java/com/example/testresults/controllers/StudentController.java
+++ src/main/java/com/example/testresults/controllers/StudentController.java
@@ -1,7 +1,9 @@
 package com.example.testresults.controllers;
 
 import com.example.testresults.objects.Student;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -11,56 +13,76 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 @RestController
 public class StudentController {
 
-    private Map<Integer, Student> studentList = new HashMap<Integer, Student>();
+
+    private ArrayList<Student> studentList = new ArrayList<>();
     private Integer id = 0;
 
     //POST that creates a student object
     @PostMapping("students")
-    public void addStudent(@RequestParam String firstName, @RequestParam String lastName, @RequestParam int testScore1, @RequestParam int testScore2, @RequestParam int testScore3) {
-        studentList.put(id, new Student(id, firstName, lastName, testScore1, testScore2, testScore3));
+    public Mono<Student> addStudent(@RequestBody Student student) {
+        return Mono.just(student).map(this::createStudent);
+    }
+
+    private Student createStudent(Student student) {
+        student.setId(id);
+        studentList.add(student);
         id++;
+        return student;
     }
 
     //PATCH that takes a studentId and a request body and updates the person with that corresponding id to all the new values provided in the body
     @PatchMapping("students/{id}")
-    public void updateStudent(@PathVariable int id, @RequestParam String firstName, @RequestParam String lastName, @RequestParam int testScore1, @RequestParam int testScore2, @RequestParam int testScore3) {
-        studentList.put(id, new Student(id, firstName, lastName, testScore1, testScore2, testScore3));
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public Mono<Void> updateStudent(@PathVariable int id, @RequestBody Student studentData) {
+        return Mono.just(id).map(this::findStudent).map(student -> {
+                student.setFirstName(studentData.getFirstName());
+                student.setLastName(studentData.getLastName());
+                student.setTestScore1(studentData.getTestScore1());
+                student.setTestScore2(studentData.getTestScore2());
+                student.setTestScore3(studentData.getTestScore3());
+                return student;
+        }).then();
+    }
+
+    private Student findStudent(int id) {
+        return studentList.stream().filter(student -> student.getId() == id).findFirst().orElse(null);
     }
 
     //GET that returns all existing students
     @GetMapping("students")
     public List<Student> getStudents() {
-        return new ArrayList<Student>(studentList.values());
+        return studentList;
     }
     //GET that returns a specific student given an Id
     @GetMapping("students/{id}")
-    public Student getStudent(@PathVariable int id) {
-        return studentList.get(id);
+    public Mono<Student> getStudent(@PathVariable int id) {
+        return Mono.just(findStudent(id));
     }
 
     //DELETE that removes a student from the service
     @DeleteMapping("students/{id}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
     public void deleteStudent(@PathVariable int id) {
-        studentList.remove(id);
+        Mono.just(id).map(this::findStudent).map(student -> studentList.remove(student));
     }
 
     //GET that returns a list of all students in ascending order based on the operation provided /students/(studentId OR testScore1 OR testScore2 OR testScore3 OR overRanking)
-    @GetMapping("students/orderedBy/{scoreType}")
+    @GetMapping("students/orderedBy/{scoreType:studentId|testScore1|testScore2|testScore3|overallRanking}")
     public List<Student> getStudentsby(@PathVariable String scoreType) {
         if(scoreType.equals("studentId")) {
-            return new ArrayList<Student>(studentList.values().stream().sorted(Comparator.comparingInt(Student::getId)).collect(Collectors.toList()));
+            return studentList.stream().sorted(Comparator.comparingInt(Student::getId).reversed()).collect(Collectors.toList());
         }
         else if(scoreType.equals("testScore1")) {
-            return new ArrayList<Student>(studentList.values().stream().sorted(Comparator.comparingInt(Student::getTestScore1)).collect(Collectors.toList()));
+            return studentList.stream().sorted(Comparator.comparingInt(Student::getTestScore1).reversed()).collect(Collectors.toList());
         }
         else if(scoreType.equals("testScore2")) {
-            return new ArrayList<Student>(studentList.values().stream().sorted(Comparator.comparingInt(Student::getTestScore2)).collect(Collectors.toList()));
+            return studentList.stream().sorted(Comparator.comparingInt(Student::getTestScore2).reversed()).collect(Collectors.toList());
         }
         else if(scoreType.equals("testScore3")) {
-            return new ArrayList<Student>(studentList.values().stream().sorted(Comparator.comparingInt(Student::getTestScore3)).collect(Collectors.toList()));
+            return studentList.stream().sorted(Comparator.comparingInt(Student::getTestScore3).reversed()).collect(Collectors.toList());
         }
         else if(scoreType.equals("overallRanking")) {
-            return new ArrayList<Student>(studentList.values().stream().sorted(Comparator.comparingInt(Student::getOverallRanking)).collect(Collectors.toList()));
+            return studentList.stream().sorted(Comparator.comparingInt(Student::getOverallRanking).reversed()).collect(Collectors.toList());
         }
         else {
             return null;
--- src/main/java/com/example/testresults/objects/Student.java
+++ src/main/java/com/example/testresults/objects/Student.java
@@ -1,5 +1,10 @@
 package com.example.testresults.objects;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder(toBuilder = true)
 public class Student {
     int id;
     String firstName;
@@ -9,68 +14,8 @@ public class Student {
     int testScore3;
     int overallRanking;
 
-    public Student(int id, String firstName, String lastName, int testScore1, int testScore2, int testScore3) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.testScore1 = testScore1;
-        this.testScore2 = testScore2;
-        this.testScore3 = testScore3;
-        this.overallRanking = testScore1 + testScore2 + testScore3;
-    }
-    public Integer getId() {
-        return id;
-    }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public Integer getTestScore1() {
-        return testScore1;
-    }
-
-    public void setTestScore1(Integer testScore1) {
-        this.testScore1 = testScore1;
-    }
-
-    public Integer getTestScore2() {
-        return testScore2;
-    }
-
-    public void setTestScore2(Integer testScore2) {
-        this.testScore2 = testScore2;
-    }
-
-    public Integer getTestScore3() {
-        return testScore3;
-    }
-
-    public void setTestScore3(Integer testScore3) {
-        this.testScore3 = testScore3;
-    }
-
-    public Integer getOverallRanking() {
-        return overallRanking;
-    }
-
-    public void setOverallRanking(Integer overallRanking) {
-        this.overallRanking = overallRanking;
+    public int getOverallRanking() {
+        return(testScore1+testScore2+testScore3);
     }
 }
